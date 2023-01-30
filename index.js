0e4917bde49b213bf5f861b7e2293301a222ba9c
plla2 <jhbljs92@naver.com>
2023-01-31 00:46:25 +0900

    second commit

--- index.js
+++ index.js
@@ -14,32 +14,34 @@ app.get("/pharmach_list", (req, res) => {
   //app.get도 비동기형식이기 때문에 동기형으로 바꾸는 작업이 필요하다.
   //pharmach_list 페이지에 접속했을때 접속한 클라이언트에게 보낼 데이터를 정할 수 있다.
   //req는 사용자로부터 전달받은 내용, res는 nodejs에서 클라이언트에게 데이터를 보내는 부분
-  let response = null; //초깃값은 null로 설정하고 아래가 실행되면 axios.get을 통해 받는다.
-  try {
-    //try로 axios를 실행하고 에러발생시 catch문을 실행한다.
-    let api = async () => {
+
+  //try로 axios를 실행하고 에러발생시 catch문을 실행한다.
+  let api = async () => {
+    let response = null; //초깃값은 null로 설정하고 아래가 실행되면 axios.get을 통해 받는다.
+    try {
       response = await axios.get(
         "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire",
         {
           params: {
             serviceKey:
-              "ID43EM%2FixQiOJbYb9kyZ3coo6DpQnKI7F8BT7LuhMdxB6s%2FNyzBVFAOlvyODT%2Fxf4aFJzFb1hYcGfEP8JrDi9A%3D%3D",
-            Q0: "서울특별시",
-            Q1: "강남구",
-            QT: "",
-            QN: "",
-            ORD: "",
-            pageNo: "1",
-            numOfRows: "1000",
+              "ID43EM/ixQiOJbYb9kyZ3coo6DpQnKI7F8BT7LuhMdxB6s/NyzBVFAOlvyODT/xf4aFJzFb1hYcGfEP8JrDi9A==",
+            Q0: req.query.Q0, //index.html (클라이언트)에서 넘겨준 데이터는 req에 있기 때문에 req를 통해 받는다.
+            Q1: req.query.Q1,
+            QT: req.query.QT,
+            QN: req.query.QN,
+            ORD: req.query.ORD,
+            pageNo: req.query.pageNo,
+            numOfRows: req.query.numOfRows,
           },
         }
       );
-    }; //nodejs를 통해 api내용을 가지고 오는 이유는 cors보안 문제를 해결하기 위해서이다. 즉, 교차된 리소스보안 문제를 해결하기 위한것.
-    api().then((response) => {
-      res.setHeader("Access-Control-Allow-Origin", "*"); //이 문장을 쓰기위해 nodejs를 쓰는것이다.(cors보안문제 해결)
-    });
-  } catch (e) {
-    console.log(e);
-  }
-  return response; //사용자가 pharmach_list에 들어왔을때 api결과가 출력된다.
+    } catch (e) {
+      console.log(e);
+    }
+    return response; //사용자가 pharmach_list에 들어왔을때 api결과가 출력된다.
+  }; //nodejs를 통해 api내용을 가지고 오는 이유는 cors보안 문제를 해결하기 위해서이다. 즉, 교차된 리소스보안 문제를 해결하기 위한것.
+  api().then((response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"); //이 문장을 쓰기위해 nodejs를 쓰는것이다.(cors보안문제 해결)
+    res.json(response.data.response.body); //response에 있는 결과물(.data.response.body 즉, 약국데이터)를 그대로 /pharmach_list경로로 접속한 컴퓨터에게 전달
+  });
 });
